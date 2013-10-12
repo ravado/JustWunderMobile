8e2ba9f7ab0ed10d1a096c5eceb700793b5f7e89
ravado <the.ravado@gmail.com>
2013-10-12 18:01:46 +0300

    add protobuf library to SAL
    add proto attributes for api models

--- JustWunderMobile.SAL/Models/ApiReleaseJoke.cs
+++ JustWunderMobile.SAL/Models/ApiReleaseJoke.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using JustWunderMobile.SAL.Interfaces;
+using ProtoBuf;
 
 namespace JustWunderMobile.SAL.Models
 {
     /// <summary>
     /// Representation of published joke from api server
     /// </summary>
+    [ProtoContract]
     public class ApiReleaseJoke : IApiReleaseJoke
     {
+        [ProtoMember(1)]
         public int Id { get; set; }
+        [ProtoMember(2)]
         public DateTime PublishDate { get; set; }
+        [ProtoMember(3)]
         public string TextJoke { get; set; }
+        [ProtoMember(4)]
         public string UserEmail { get; set; }
+        [ProtoMember(5)]
         public int Rating { get; set; }
+        [ProtoMember(6)]
         public bool Censorship { get; set; }
     }
 }
