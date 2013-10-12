555c6b750dc1d5e5ced5a70112b119a70d302ccb
ravado <the.ravado@gmail.com>
2013-10-12 17:39:49 +0300

    add JustWunderApiService (not implemented)

---
+++ JustWunderMobile.SAL/Services/JustWunderApiService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using JustWunderMobile.SAL.Interfaces;
+
+namespace JustWunderMobile.SAL.Services
+{
+    /// <summary>
+    /// ApiService implementation of JustWunder.com API
+    /// </summary>
+    public class JustWunderApiService : IApiService
+    {
+        public IEnumerable<IApiReleaseJoke> GetAllReleasedJokes()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IApiReleaseJoke> GetNewJokes(int lastJokeId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PostNewJoke(IApiInboxJoke newJoke)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
