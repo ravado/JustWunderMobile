295540a42ca531c147b9895779850b399c1f160c
Ivan Cherednychok <ravadog@gmail.com>
2013-12-12 23:36:27 +0200

    add application bar for main page
    create commands for refresh and for open settings page

--- JustWunderMobile.Common/ViewModels/MainViewModel.cs
+++ JustWunderMobile.Common/ViewModels/MainViewModel.cs
@@ -2,11 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 using Cirrious.MvvmCross.ViewModels;
 
 namespace JustWunderMobile.Common.ViewModels
 {
     public class MainViewModel : MvxViewModel
     {
+        // commands
+        private MvxCommand _refreshCommand;
+        private MvxCommand showSettingsCommand;
+
+        // commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                _refreshCommand = _refreshCommand ?? new MvxCommand(Refresh);
+                return _refreshCommand;
+            }
+        }
+        public ICommand ShowSettingsCommand
+        {
+            get
+            {
+                showSettingsCommand = showSettingsCommand ?? new MvxCommand(ShowSettings);
+                return showSettingsCommand;
+            }
+        }
+
+        private void ShowSettings()
+        {
+            System.Diagnostics.Debug.WriteLine("SETTINGS...");
+        }
+
+        private void Refresh()
+        {
+            System.Diagnostics.Debug.WriteLine("REFRESHING...");
+        }
     }
 }
