37e131bbcac5b9374accb25ea0c3d6aa0bae6959
Ivan Cherednychok <ravadog@gmail.com>
2013-12-17 23:06:32 +0200

    start implementing multiselector control

--- JustWunderMobile.Common/ViewModels/MainViewModel.cs
+++ JustWunderMobile.Common/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using Cirrious.MvvmCross.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Cirrious.MvvmCross.ViewModels;
 using System.Windows.Input;
+using JustWunderMobile.Common.DataModels;
 using JustWunderMobile.Common.Resources;
 
 namespace JustWunderMobile.Common.ViewModels
@@ -14,9 +16,14 @@ namespace JustWunderMobile.Common.ViewModels
         private MvxCommand _showAboutCommand;
         #endregion
 
+        private ObservableCollection<ReleaseJokeDataModel> _newJokes;
+        private ObservableCollection<ReleaseJokeDataModel> _topJokes;
+        private ObservableCollection<ReleaseJokeDataModel> _favoriteJokes;
+
         #endregion
 
         #region Properties
+
 
         public override string PageName
         {
@@ -47,6 +54,38 @@ namespace JustWunderMobile.Common.ViewModels
             get { return UILabels.MainPage_Menu_Refresh; }
         }
 
+        public ObservableCollection<ReleaseJokeDataModel> NewJokes
+        {
+            get { return _newJokes ?? (_newJokes = new ObservableCollection<ReleaseJokeDataModel>()); }
+            set
+            {
+                _newJokes = value;
+                RaisePropertyChanged(() => NewJokes);
+
+            }
+        }
+        public ObservableCollection<ReleaseJokeDataModel> TopJokes
+        {
+            get { return _topJokes ?? (_topJokes = new ObservableCollection<ReleaseJokeDataModel>()); }
+            set
+            {
+                _topJokes = value;
+                RaisePropertyChanged(() => TopJokes);
+
+            }
+        }
+        public ObservableCollection<ReleaseJokeDataModel> FavoriteJokes
+        {
+            get { return _favoriteJokes ?? (_favoriteJokes = new ObservableCollection<ReleaseJokeDataModel>()); }
+            set
+            {
+                _favoriteJokes = value;
+                RaisePropertyChanged(() => FavoriteJokes);
+
+            }
+        }
+
+
         #region Commands
         public ICommand RefreshCommand
         {
@@ -76,6 +115,11 @@ namespace JustWunderMobile.Common.ViewModels
 
         #endregion
 
+        public MainViewModel()
+        {
+            LoadFakeData();
+        }
+
         #region Methods
 
         private void ShowAbout()
@@ -91,6 +135,136 @@ namespace JustWunderMobile.Common.ViewModels
             System.Diagnostics.Debug.WriteLine("REFRESHING...");
         }
 
+        private void LoadFakeData()
+        {
+            #region NewJokes
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Я родом из Орехово-Зуево. И я просто ненавижу русскую раскладку за то, что буквы З и Х расположены рядом."
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Когда-то давно я посадила в горшок орех из Рафаэлло. Прошло 11 лет, а я до сих пор надеюсь, что из него вырастет Рафаэлловое дерево :)"
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Это просто вопрос времени, когда к моему имени и фамилии добавят слово «синдром»."
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Отец так хотел, чтобы его сын стал физиком, что бил его не ремнем, а током."
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Скажи мне, сколько языков программирования ты знаешь, и я скажу, носишь ли ты сандали с носками."
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Ищу девушку с квартирой для очень серьёзных отношений, машина у меня уже есть от предыдущей жены."
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Мальчик, которого воспитали бараны, без проблем адаптировался в современном обществе."
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Если ты, когда-нибудь, лежа на девушке, заметишь, что глаза ее лихорадочно блестят, губы влажны и чувственны, а тело дрожит как осиновый лист на ветру - слазь с нее и беги... Это - малярия!"
+            });
+            NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Коллеги, сегодня я вас всех собрал, потому что вы - пазл."
+            }); NewJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Маша любила петь... eё рекорд был 40 Петь в месяц."
+            });
+            #endregion
+
+            #region TopJokes
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Я родом из Орехово-Зуево. И я просто ненавижу русскую раскладку за то, что буквы З и Х расположены рядом."
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Когда-то давно я посадила в горшок орех из Рафаэлло. Прошло 11 лет, а я до сих пор надеюсь, что из него вырастет Рафаэлловое дерево :)"
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Это просто вопрос времени, когда к моему имени и фамилии добавят слово «синдром»."
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Отец так хотел, чтобы его сын стал физиком, что бил его не ремнем, а током."
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Скажи мне, сколько языков программирования ты знаешь, и я скажу, носишь ли ты сандали с носками."
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Ищу девушку с квартирой для очень серьёзных отношений, машина у меня уже есть от предыдущей жены."
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Мальчик, которого воспитали бараны, без проблем адаптировался в современном обществе."
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Если ты, когда-нибудь, лежа на девушке, заметишь, что глаза ее лихорадочно блестят, губы влажны и чувственны, а тело дрожит как осиновый лист на ветру - слазь с нее и беги... Это - малярия!"
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Коллеги, сегодня я вас всех собрал, потому что вы - пазл."
+            });
+            TopJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Маша любила петь... eё рекорд был 40 Петь в месяц."
+            });
+            #endregion
+
+            #region TopJokes
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Я родом из Орехово-Зуево. И я просто ненавижу русскую раскладку за то, что буквы З и Х расположены рядом."
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Когда-то давно я посадила в горшок орех из Рафаэлло. Прошло 11 лет, а я до сих пор надеюсь, что из него вырастет Рафаэлловое дерево :)"
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Это просто вопрос времени, когда к моему имени и фамилии добавят слово «синдром»."
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Отец так хотел, чтобы его сын стал физиком, что бил его не ремнем, а током."
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Скажи мне, сколько языков программирования ты знаешь, и я скажу, носишь ли ты сандали с носками."
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Ищу девушку с квартирой для очень серьёзных отношений, машина у меня уже есть от предыдущей жены."
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Мальчик, которого воспитали бараны, без проблем адаптировался в современном обществе."
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Если ты, когда-нибудь, лежа на девушке, заметишь, что глаза ее лихорадочно блестят, губы влажны и чувственны, а тело дрожит как осиновый лист на ветру - слазь с нее и беги... Это - малярия!"
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Коллеги, сегодня я вас всех собрал, потому что вы - пазл."
+            });
+            FavoriteJokes.Add(new ReleaseJokeDataModel()
+            {
+                TextJoke = "Маша любила петь... eё рекорд был 40 Петь в месяц."
+            });
+            #endregion
+        }
         #endregion
     }
 }
