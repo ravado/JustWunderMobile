7a88a6daff7d94012bc21f2141c87ac53d1deeff
Ivan Cherednychok <ravadog@gmail.com>
2014-04-01 22:05:06 +0300

    add visual state for favorite jokes

---
+++ JustWunderMobile.Phone/Classes/Converters/FavoriteConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace JustWunderMobile.Phone.Classes.Converters
+{
+    public class FavoriteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                var v = (bool) value;
+
+                if (v)
+                {
+                    return "/Assets/AppBar/outline_star-32_filled.png";
+                    
+                }
+            }
+
+            return "/Assets/AppBar/outline_star-32.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
