f0a17a64cb9c52397651f176a05130b2ed184ee0
Ivan Cherednychok <ravadog@gmail.com>
2014-05-02 13:19:04 +0300

    fixed wrong selected item state
    add visibility convertor and visual state resources for
    LongListMultiSelector

---
+++ JustWunderMobile.Phone/Classes/Converters/VisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace JustWunderMobile.Phone.Classes.Converters
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var converted = value as bool?;
+            if (value != null && converted != null && converted.Value)
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var converted = value as Visibility?;
+            if (value != null && converted != null && converted == Visibility.Visible)
+                return true;
+
+            return false;
+        }
+    }
+}
